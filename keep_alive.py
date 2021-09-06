94116d40f241c140767a7e61df1cd2da050371d1
ironman9356 <75370527+ironman9356@users.noreply.github.com>
2021-09-06 19:06:05 +0530

    Reformat entire code

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template
 from threading import Thread
 
-
-
+from flask import Flask, render_template
 
 app = Flask('')
 
+
 @app.route('/')
 def index():
-   return render_template('index.html')
+    return render_template('index.html')
+
+
 def run():
     app.run(host="0.0.0.0", port=8080)
+
+
 def keep_alive():
     server = Thread(target=run)
     server.start()
-
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import discord
 import os
+
+import discord
 from discord.ext import commands
+
 from keep_alive import keep_alive
 
 keep_alive()
 
 intents = discord.Intents.all()
+
+
 class ArtistBot(commands.Bot):
     def __init__(self):
         super().__init__(
@@ -14,12 +18,12 @@ class ArtistBot(commands.Bot):
                 548530397034577930,
                 580385287297957888,
                 732143432998322267
-                },
+            },
             case_insensitive=True,
             intents=intents,
             strip_after_prefix=True,
             self_bot=False,
-           activity=discord.Activity(type=discord.ActivityType.watching, name='kristian lose his mind ')
+            activity=discord.Activity(type=discord.ActivityType.watching, name='kristian lose his mind ')
         )
 
     async def on_ready(self):
@@ -30,34 +34,32 @@ class ArtistBot(commands.Bot):
 
 bot = ArtistBot()
 
+
 @bot.command(help="To load a Cog")
 @commands.is_owner()
-async def load(ctx,cog):
-  try:
-    bot.load_extension(f'cogs.{cog}')
-    await ctx.send(f'Loaded {cog}')
-  except commands.ExtensionNotLoaded:
-    await ctx.send('That extention is already loaded')
-
+async def load(ctx, cog):
+    try:
+        bot.load_extension(f'cogs.{cog}')
+        await ctx.send(f'Loaded {cog}')
+    except commands.ExtensionNotLoaded:
+        await ctx.send('That extention is already loaded')
 
 
 @bot.command(help="To unload a Cog")
 @commands.is_owner()
-async def unload(ctx,cog):
-  try:
-    bot.unload_extension(f'cogs.{cog}')
-    await ctx.send(f'Unloaded {cog}')
-  except commands.ExtensionNotLoaded:
-    await ctx.send('That extention is already unloaded')
+async def unload(ctx, cog):
+    try:
+        bot.unload_extension(f'cogs.{cog}')
+        await ctx.send(f'Unloaded {cog}')
+    except commands.ExtensionNotLoaded:
+        await ctx.send('That extention is already unloaded')
 
 
-    
 @bot.command(help="To reload a Cog")
 @commands.is_owner()
-async def reload(ctx,cog):
+async def reload(ctx, cog):
     bot.reload_extension(f"cogs.{cog}")
     await ctx.send(f"{cog} was reloaded")
-
 
 
 for filename in os.listdir('./cogs'):
